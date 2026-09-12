3431bd8dbad432edf61748b426aeb0fff0df9456
KAS <65353028+dev-kas@users.noreply.github.com>
2026-09-13 03:47:04 +0530

    Update redirect URL in main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/*", (req, res) => {
-	res.status(301).redirect("https://v3.talkium.in" + req.path)
+	res.status(301).redirect("https://app.talkium.in" + req.path)
 })
 
 app.listen(80);
